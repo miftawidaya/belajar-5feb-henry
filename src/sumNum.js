dc7da64b16136bd6d7ac93ba42f69ad804f89727
miftawidaya <miftawidaya@gmail.com>
2025-02-05 18:44:14 +0700

    Sum of Two Numbers

---
+++ src/sumNum.js
@@ -0,0 +1,6 @@
+/* Sum of Two Numbers
+Diberikan sebuah array nums dan sebuah angka target, temukan dua angka dalam array yang jika dijumlahkan hasilnya adalah target. Kembalikan index kedua angka tersebut.
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1] (Karena nums[0] + nums[1] == 9)
+Input: nums = [3, 2, 4], target = 6
+Output: [1, 2] */
